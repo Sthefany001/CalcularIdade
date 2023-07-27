5115d4c93d526c13c6cde292cfa09b97b866a770
Sthefany <s532401ed@gmail.com>
2023-07-27 11:25:36 -0400

    first commit

---
+++ 01 - Idade/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01___Idade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Text = "";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime dataNasc = dateTimePicker1.Value;
+            //int idade = DateTime.Today.Year - dataNasc.Year;
+
+            TimeSpan diasIdade = dataNasc.Subtract(DateTime.Today);
+            //label2.Text = diasIdade.Days.ToString();
+
+            double idadeOfc = diasIdade.Days / 365.2425;
+            idadeOfc = Math.Floor(idadeOfc) * (-1);
+            label2.Text = idadeOfc.ToString("0");
+            
+        }
+    }
+}
